ac5d6cbbbe3d77217731461429efec87fee38e44
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2025-05-21 04:13:36 -0400

    feat: Adicionado imagens de produtos e paginação.

--- produto/views.py
+++ produto/views.py
@@ -1,24 +1,35 @@
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
 
-from categoria.admin import Categoria
-from produto.admin import Produto
+from categoria.models import Categoria
+from produto.models import Produto
 
 # Create your views here.
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
+from categoria.models import Categoria
+from produto.models import Produto
+
 def visualizarLoja(request, categoria_slug=None):
-    categorias = None
-    produtos = None
+    categoria = None
     
-    if categoria_slug != None:
-        categorias = get_object_or_404(Categoria, slug = categoria_slug)
-        produtos = Produto.objects.filter(categoria = categorias, esta_disponivel = True)
-        produto_quant = produtos.count()
+    if categoria_slug:
+        categoria = get_object_or_404(Categoria, slug=categoria_slug)
+        produtos_list = Produto.objects.filter(categoria=categoria, esta_disponivel=True)
     else:
-        produtos = Produto.objects.all().filter(esta_disponivel = True)
-        produto_quant = produtos.count()
-        
+        produtos_list = Produto.objects.filter(esta_disponivel=True)
+
+    paginator = Paginator(produtos_list, 9)  # 9 produtos por página
+    pagina_num = request.GET.get('page')
+    produtos = paginator.get_page(pagina_num)
+
     context = {
-        'produtos':produtos,
-        'produto_quant':produto_quant,
+        'produtos': produtos,
+        'produto_quant': paginator.count,
+        'categoria_atual': categoria,
     }
     
     return render(request, 'shop-grid.html', context)
+
+    
+    
