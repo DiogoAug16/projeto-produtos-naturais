8676478b70a9338cfb296b17931b5160ecf0b353
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2025-06-04 01:04:05 -0400

    feat: adicionado visualização do produto.

--- produto/urls.py
+++ produto/urls.py
@@ -5,4 +5,5 @@ from produto import views
 urlpatterns = [
     path('', views.visualizarLoja, name='shop'),
     path('<slug:categoria_slug>/', views.visualizarLoja, name='produtos_por_categoria'),
+    path('<slug:categoria_slug>/<slug:produto_slug>/', views.visualizarDetalheProduto, name='visualizarDetalheProduto'),
 ]
