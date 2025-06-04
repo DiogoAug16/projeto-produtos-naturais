2f4593b7ad9785a080cd9b29089a074c7ac810df
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2025-06-04 03:07:20 -0400

    feat: adicionado list display de departamento em produto

--- produto/admin.py
+++ produto/admin.py
@@ -8,9 +8,13 @@ class ProdutoAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         'slug': ('produto_nome',)
     }
-    list_display = ('produto_nome', 'slug', 'preco', 'promocao_valor_porcentagem', 'estoque', 'esta_disponivel','promocao_disponivel', 'categoria',)
+    list_display = ('produto_nome', 'slug', 'preco', 'promocao_valor_porcentagem', 'estoque', 'esta_disponivel','promocao_disponivel', 'categoria','get_departamento')
     list_filter = ('esta_disponivel', 'promocao_disponivel')
     search_fields = ('produto_nome',)
     list_editable = ('preco','promocao_valor_porcentagem', 'estoque', 'esta_disponivel', 'promocao_disponivel')
+    
+    def get_departamento(self, obj):
+        return obj.categoria.departamento.nome
+    get_departamento.short_description = 'Departamento'
 
 admin.site.register(Produto, ProdutoAdmin)
