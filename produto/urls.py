3f55cec3e3649947c69c21bb481920252b979ad2
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2025-06-05 03:28:15 -0400

    feat: adicionado função de calcular o frete na visualização do produto

--- produto/urls.py
+++ produto/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('departamento/<slug:departamento_slug>/categoria/<slug:categoria_slug>/', views.visualizarLoja, name='produtos_por_categoria'),
     path('departamento/<slug:departamento_slug>/', views.visualizarLoja, name='produtos_por_departamento'),
     path('departamento/<slug:departamento_slug>/categoria/<slug:categoria_slug>/<slug:produto_slug>/', views.visualizarDetalheProduto, name='visualizarDetalheProduto'),
+    path('calcular-frete/', views.calcularFrete, name='calcularFrete'),
 ]
