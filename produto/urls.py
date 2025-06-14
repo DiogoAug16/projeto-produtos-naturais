f24c35a0180a1a133799b8f1c8171058d1f283c1
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2025-06-14 04:34:06 -0400

    feat: adicionado função de busca no site

--- produto/urls.py
+++ produto/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('departamento/<slug:departamento_slug>/', views.visualizarLoja, name='produtos_por_departamento'),
     path('departamento/<slug:departamento_slug>/categoria/<slug:categoria_slug>/<slug:produto_slug>/', views.visualizarDetalheProduto, name='visualizarDetalheProduto'),
     path('calcular-frete/', views.calcularFrete, name='calcularFrete'),
+    path('busca/<str:keyword>/', views.visualizarLoja, name='search_by_keyword'),
+    path('busca/', views.visualizarLoja, name='search_base'),
+
+
 ]
--- produtos_naturais/urls.py
+++ produtos_naturais/urls.py
@@ -19,11 +19,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 
-import produto
 from produtos_naturais import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('shop/', include('produto.urls')),
+    path('loja/', include('produto.urls')),
+    
 ] + static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
