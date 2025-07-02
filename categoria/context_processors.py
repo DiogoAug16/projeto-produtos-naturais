e5d2b117b0c4fab11c7333d67481771e1bcdf338
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2025-07-02 03:08:11 -0400

    refactor: renomeia chave de retorno no processador de contexto de categorias

--- categoria/context_processors.py
+++ categoria/context_processors.py
@@ -3,4 +3,4 @@ from categoria.admin import Categoria
 
 def menu_categoria(request):
     lista_categoria = Categoria.objects.all()
-    return dict(opcoes = lista_categoria)
+    return dict(opcoes_cat = lista_categoria)
