e8e141107e8da71c079387a6c968cd73349c4b76
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2025-07-02 21:50:32 -0400

    feat: adicionado o csrf no settings para o token de segurança

--- produtos_naturais/settings.py
+++ produtos_naturais/settings.py
@@ -55,6 +55,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://testesiteecommerceprogweb.azurewebsites.net',
+]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'produtos_naturais.urls'
 
