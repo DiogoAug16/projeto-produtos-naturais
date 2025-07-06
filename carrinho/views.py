615f833bb5d9940fd63a129e64de6b21a7610050
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2025-07-06 00:24:30 -0400

    feat: adicionar suporte a usuários no carrinho e favoritos

--- carrinho/views.py
+++ carrinho/views.py
@@ -1,54 +1,43 @@
+# carrinho/views.py
 from django.shortcuts import get_object_or_404, redirect, render
-
-from carrinho.models import CarItem, Carrinho
-from django.core.exceptions import ObjectDoesNotExist
-
+from .models import CarItem, Carrinho
 from produto.models import Produto
 
-# Create your views here.
+def _get_or_create_carrinho(request):
+    if request.user.is_authenticated:
+        carrinho, _ = Carrinho.objects.get_or_create(user=request.user)
+    else:
+        session_key = request.session.session_key
+        if not session_key:
+            session_key = request.session.create()
+        carrinho, _ = Carrinho.objects.get_or_create(car_id=session_key)
+    return carrinho
 
-def getCarId(request):
-    carSession = request.session.session_key
-    if not carSession:
-        carSession = request.session.create()
-        carSession = request.session.session_key  # <- importante
-    return carSession
-
-def visualizarCarrinho(request, total = 0, quantidade = 0, car_items = None, imposto = 0, total_com_imposto = 0):
+def visualizarCarrinho(request, total=0, quantidade=0, imposto=0, total_com_imposto=0):
+    car_items = []
     try:
-        car = Carrinho.objects.get(car_id = getCarId(request))
-        car_items = CarItem.objects.filter(carrinho = car, esta_disponivel = True)
+        carrinho = _get_or_create_carrinho(request)
+        car_items = CarItem.objects.filter(carrinho=carrinho, esta_disponivel=True)
         for item in car_items:
-            if item.produto.promocao_disponivel and item.produto.promocao_valor_porcentagem > 0:
-                total += (item.produto.preco_com_desconto() * item.quantidade)
-                imposto += (item.produto.preco_com_desconto() * item.quantidade) * (item.produto.imposto/100)
-            else:
-                total += (item.produto.preco * item.quantidade)
-                imposto += (item.produto.preco * item.quantidade) * (item.produto.imposto/100)
-            total_com_imposto = total + imposto
+            total += item.getSubTotal()
+            imposto += item.getSubTotal() * (item.produto.imposto / 100)
             quantidade += item.quantidade
-    except ObjectDoesNotExist:
-        pass
-    
+        total_com_imposto = total + imposto
+    except Carrinho.DoesNotExist:
+        pass  # Se o carrinho não existe, as listas ficarão vazias
+
     context = {
         'imposto': imposto,
         'total': total,
         'total_com_imposto': total_com_imposto,
         'car_items': car_items,
     }
-    
-    return render(request, 'shoping-cart.html', context)
+    return render(request, 'loja/shoping-cart.html', context)
 
 def adicionarItemCarrinho(request, produto_id):
-    produto = Produto.objects.get(id=produto_id)
+    produto = get_object_or_404(Produto, id=produto_id)
     quantidade = int(request.POST.get('quantidade', 1))
-    try:
-        carrinho = Carrinho.objects.get(car_id = getCarId(request))
-    except Carrinho.DoesNotExist:
-        carrinho = Carrinho.objects.create(
-            car_id = getCarId(request)
-        )
-    carrinho.save()
+    carrinho = _get_or_create_carrinho(request)
 
     try:
         car_item = CarItem.objects.get(produto=produto, carrinho=carrinho)
@@ -56,28 +45,28 @@ def adicionarItemCarrinho(request, produto_id):
         car_item.save()
     except CarItem.DoesNotExist:
         car_item = CarItem.objects.create(
-            produto = produto,
-            quantidade = quantidade,
-            carrinho = carrinho,
+            produto=produto,
+            quantidade=quantidade,
+            carrinho=carrinho,
         )
-        car_item.save()
     return redirect('carrinho')
 
 def diminuirQuantidadeProdutoCarrinho(request, produto_id):
-    car = Carrinho.objects.get(car_id = getCarId(request))
+    carrinho = _get_or_create_carrinho(request)
     produto = get_object_or_404(Produto, id=produto_id)
-    car_item = CarItem.objects.get(produto=produto, carrinho=car)
-    if car_item.quantidade > 1:
-        car_item.quantidade -= 1
-        car_item.save()
-    else:
-        car_item.delete()
+    try:
+        car_item = CarItem.objects.get(produto=produto, carrinho=carrinho)
+        if car_item.quantidade > 1:
+            car_item.quantidade -= 1
+            car_item.save()
+        else:
+            car_item.delete()
+    except CarItem.DoesNotExist:
+        pass
     return redirect('carrinho')
 
 def removerItemCarrinho(request, produto_id):
-    car = Carrinho.objects.get(car_id = getCarId(request))
+    carrinho = _get_or_create_carrinho(request)
     produto = get_object_or_404(Produto, id=produto_id)
-    car_item = CarItem.objects.get(produto=produto, carrinho=car)
-    if car_item.quantidade >= 1:
-        car_item.delete()
+    CarItem.objects.filter(produto=produto, carrinho=carrinho).delete()
     return redirect('carrinho')
