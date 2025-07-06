3e9421530395193d6bf12fc6e012671bf1ad291c
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2025-07-06 00:24:53 -0400

    feat: adicionar suporte a usuários nos favoritos e refatorar funções de manipulação

--- favoritos/models.py
+++ favoritos/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 from produto.models import Produto
 
 # Create your models here.
 
 class Favoritos(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     fav_id = models.CharField(max_length=250, blank=True)
     data_criado = models.DateField(auto_now_add=True)
     
