09763371287c505047e96c151bb5c6de5988a21b
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2025-07-06 02:24:11 -0400

    feat: implementaçao inicial do checkout

--- produtos_naturais/urls.py
+++ produtos_naturais/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('carrinho/', include ('carrinho.urls')),
     path('favoritos/', include ('favoritos.urls')),
     path('login/', include('login_usuario.urls')),
+    path('checkout/', include('checkout.urls')),
     
 ] + static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
