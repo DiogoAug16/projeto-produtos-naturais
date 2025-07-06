0037e0156db3a2ed73a79acefb078dba8799a28c
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2025-07-06 00:24:30 -0400

    feat: adicionar suporte a usuários no carrinho e favoritos

--- carrinho/context_processors.py
+++ carrinho/context_processors.py
@@ -1,37 +1,67 @@
+# carrinho/context_processors.py
 
-from carrinho.models import CarItem, Carrinho
-from carrinho.views import getCarId
+from .models import CarItem, Carrinho
+from favoritos.models import FavItem, Favoritos
 
+def menu_context(request):
+    """
+    Um único context processor para fornecer os contadores do carrinho e favoritos,
+    e o valor total do carrinho para todas as páginas.
+    """
+    car_contador = 0
+    fav_contador = 0
+    valor_total_carrinho = 0.0
 
-def contador(request):
-    contador = 0
+    if 'admin' in request.path:
+        return {}
+
     try:
-        car = Carrinho.objects.filter(car_id = getCarId(request))
-        car_items = CarItem.objects.all().filter(carrinho=car[:1])
+        if request.user.is_authenticated:
+            try:
+                carrinho = Carrinho.objects.get(user=request.user)
+                car_items = CarItem.objects.filter(carrinho=carrinho)
+            except Carrinho.DoesNotExist:
+                car_items = CarItem.objects.none()
 
-        for car_item in car_items:
-            contador += car_item.quantidade
-    except Carrinho.DoesNotExist:
-        contador = 0
-    return dict(car_contador = contador)
+            try:
+                favoritos = Favoritos.objects.get(user=request.user)
+                fav_items = FavItem.objects.filter(favoritos=favoritos)
+            except Favoritos.DoesNotExist:
+                fav_items = FavItem.objects.none()
+        else:
+            session_key = request.session.session_key
+            if session_key:
+                try:
+                    carrinho = Carrinho.objects.get(car_id=session_key)
+                    car_items = CarItem.objects.filter(carrinho=carrinho)
+                except Carrinho.DoesNotExist:
+                    car_items = CarItem.objects.none()
 
-def valorTotalCarrinho(request):
-    total = 0
-    imposto = 0
-    
-    try:
-        car = Carrinho.objects.filter(car_id=getCarId(request))
-        car_items = CarItem.objects.all().filter(carrinho=car[:1])
-
-        for car_item in car_items:
-            if car_item.produto.promocao_disponivel and car_item.produto.promocao_valor_porcentagem > 0:
-                total += car_item.produto.preco_com_desconto() * car_item.quantidade
-                imposto += (car_item.produto.preco_com_desconto() * car_item.quantidade) * (car_item.produto.imposto/100)
+                try:
+                    favoritos = Favoritos.objects.get(fav_id=session_key)
+                    fav_items = FavItem.objects.filter(favoritos=favoritos)
+                except Favoritos.DoesNotExist:
+                    fav_items = FavItem.objects.none()
             else:
-                total += car_item.produto.preco * car_item.quantidade   
-                imposto += (car_item.produto.preco * car_item.quantidade) * (car_item.produto.imposto/100)
-        total_com_imposto = total + imposto
-    except Carrinho.DoesNotExist:
+                car_items = CarItem.objects.none()
+                fav_items = FavItem.objects.none()
+
         total = 0
         imposto = 0
-    return dict(valor_total_carrinho=total_com_imposto)
+        for item in car_items:
+            car_contador += item.quantidade
+            subtotal = item.getSubTotal()
+            total += subtotal
+            imposto += subtotal * (item.produto.imposto / 100)
+        valor_total_carrinho = total + imposto
+        
+        fav_contador = fav_items.count()
+
+    except Exception:
+        pass
+
+    return {
+        'car_contador': car_contador,
+        'fav_contador': fav_contador,
+        'valor_total_carrinho': valor_total_carrinho,
+    }
